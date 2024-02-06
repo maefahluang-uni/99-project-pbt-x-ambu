61550a925c8a96f68467a8ad9fdef4c3049b3169
Kittipong Wongsaditsayanon <94077639+Posident01@users.noreply.github.com>
2024-02-06 22:31:20 +0700

    Delete myambulancetest directory

--- myambulancetest/plugins/vuetify.js
+++
@@ -1,9 +0,0 @@
-// plugins/vuetify.js
-import Vue from "vue";
-import Vuetify from "vuetify/lib/framework";
-
-Vue.use(Vuetify);
-
-export default new Vuetify({
-  // Your Vuetify configuration options here
-});
