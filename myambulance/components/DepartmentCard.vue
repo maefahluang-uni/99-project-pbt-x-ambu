f99799f7eb2eb3703caa9346920bd9796bd2bdcd
Kittipong Wongsaditsayanon <94077639+Posident01@users.noreply.github.com>
2024-02-06 16:34:55 +0000

    setup my docker

--- myambulance/components/DepartmentCard.vue
+++ myambulance/components/DepartmentCard.vue
@@ -46,16 +46,12 @@
             </v-row>
             <!-- Additional Rows -->        
         </v-container>
-        <BarChart />
     </div>
 </template>
   
 <script>
- import BarChart from '../components/BarChart.vue'
 export default {
-    components: {
-      BarChart
-    },
+    
     computed: {
         patientCount() {
             return this.$store.state.patientCount;
